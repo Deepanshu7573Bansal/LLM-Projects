501915fa16ad522c4c008bf74e8d79727d2ed0ec
Deepanshu7573Bansal <155408232+Deepanshu7573Bansal@users.noreply.github.com>
2025-03-14 16:40:53 +0530

    Add files via upload

---
+++ AI Blogging Assistant/apikey.py
@@ -0,0 +1,8 @@
+# open_api_keys=""
+# google_gemini_api_keys="AIzaSyAqXtQUcfgjIM0-K0oHSX7U8SkYx0wq2o4"
+
+# python -m venv myvenv
+# myenv\Scripts\activate
+# pip install open streamlit
+# pip install streamlit
+# streamlit run app.py
